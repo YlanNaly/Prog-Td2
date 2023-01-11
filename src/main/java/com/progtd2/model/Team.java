cba2806cf660a903514c4e7b04ce4de351d83909
ylan <hei.nalisoa@gmail.com>
2023-01-11 14:12:44 +0300

    feat: goal component added in api.yaml

--- src/main/java/com/progtd2/model/Team.java
+++ src/main/java/com/progtd2/model/Team.java
@@ -23,6 +23,7 @@ public class Team implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   @Column(nullable = false)
   private String name;
 }
